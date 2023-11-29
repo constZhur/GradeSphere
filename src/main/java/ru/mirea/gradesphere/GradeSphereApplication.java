bc8a447c291441e79eec04e46edbf308196a5179
Konstantin Zhuravlev <113041604+constZhur@users.noreply.github.com>
2023-11-29 10:45:43 +0300

    Docker were added

---
+++ src/main/java/ru/mirea/gradesphere/GradeSphereApplication.java
@@ -0,0 +1,13 @@
+package ru.mirea.gradesphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeSphereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradeSphereApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/mirea/gradesphere/GradeSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mirea.gradesphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeSphereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
