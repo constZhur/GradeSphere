8ad1b978cd60b2fa990e1500e1d1132674225200
Konstantin Zhuravlev <113041604+constZhur@users.noreply.github.com>
2023-12-04 20:56:53 +0300

    Unused classes have been removed

--- src/main/java/ru/mirea/gradesphere/repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.mirea.gradesphere.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.mirea.gradesphere.model.users.Admin;
-
-@Repository
-public interface AdminRepository extends JpaRepository<Admin, Long> {
-}
--- src/main/java/ru/mirea/gradesphere/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.mirea.gradesphere.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.mirea.gradesphere.model.users.Student;
-
-@Repository
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
